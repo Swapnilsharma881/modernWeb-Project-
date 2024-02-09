0ab334744f7ce7c258c2b938438019da814ea127
swapnilsharma881 <swapnilsharma881@gmail.com>
2024-02-09 11:32:59 +0530

    Ready for remote push

--- index.js
+++ index.js
@@ -35,30 +35,4 @@ Shery.imageEffect("#back", {
   gooey: true,
 });
 
-var elem = document.querySelectorAll(".elem");
 
-elem.forEach(function (elem) {
-  var h1s = elem.querySelectorAll("h1");
-  var index = 0;
-  var animating = false;
-  document.querySelector("#main").addEventListener("click", function () {
-    if (!animating) {
-      animating = true;
-      gsap.to(h1s[index], {
-        top: -100,
-        duration: 1.5,
-        ease: Expo.easeInOut,
-        onComplete: function () {
-          gsap.set(this._targets, { top: 100 });
-          animating = false;
-        },
-      });
-      index === h1s.length - 1 ? (index = 0) : index++;
-      gsap.to(h1s[index], {
-        top: 0,
-        duration: 1.5,
-        ease: Expo.easeInOut,
-      });
-    }
-  });
-});
